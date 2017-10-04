c7768d4cd8d80665ca560a3a35268f8bf1bf9e31
binaymishramca@gmail.com <binaymishramca@gmail.com>
2017-10-04 21:15:20 +0530

    PersonHandler function. GET method

---
+++ src/main/java/com/serverless/PersonHandler.java
@@ -0,0 +1,24 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class PersonHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private static final Logger LOG = Logger.getLogger(PersonHandler.class);
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("received: " + input);
+        Response responseBody = new Response("Hi Binay ! Your function executed successfully!", input);
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(responseBody)
+                .setHeaders( Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .build();
+    }
+}
